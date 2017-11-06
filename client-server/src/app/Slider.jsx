8d9cfc606ac683cc8feb7208105fe0f78c7492f7
Aaron <aaron@fireislandferries.com>
2017-11-06 16:33:24 -0500

    Added interval to slider

--- client-server/src/app/Slider.jsx
+++ client-server/src/app/Slider.jsx
@@ -11,10 +11,24 @@ class Slider extends React.Component {
     };
   }
 
+  componentDidMount() {
+    console.log(this.state);
+    setInterval(this.incrementSlideCount.bind(this), 3000);
+  }
+
+  incrementSlideCount() {
+    let newSlide = this.state.slideCount + 1;
+    if(newSlide > 3) {
+      newSlide = 1;
+    }
+    this.setState({slideCount: newSlide});
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="slider">
-        test
+        Slide: { this.state.slideCount }
         { this.state.slideCount === 1 ? <ImageSlide imageUrl='assets/img/yosemite.jpg' /> : null }
         { this.state.slideCount === 2 ? <ImageSlide imageUrl='assets/img/rocks.jpg' /> : null }
         { this.state.slideCount === 3 ? <ImageSlide imageUrl='assets/img/flowers.jpg' /> : null }
