206c78853891f50a03608f1bdbeaac8c6e19ad45
Anthony Bonventre <abonventre@gmail.com>
2017-11-06 12:21:00 -0500

    Fixed path to static in client-server

--- client-server/app.js
+++ client-server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const rover = 'http://localhost:3000';
 const app = express();
+const path = require('path');
 const port = 3010;
 const server = require('http').createServer(app);
 const socket = require('socket.io-client')(rover);
 
-app.use('/static', express.static('src'));
+app.use('/static', express.static(path.join(__dirname, 'src')));
 app.get('/', (req, res) => res.send('Welcome to the roverDigital Client'));
 
 
