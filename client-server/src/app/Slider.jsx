224c8bc9c1b531af50955ae148686eb3ee00aeb6
Aaron <aaron@fireislandferries.com>
2017-11-14 15:51:55 -0500

    Imported imageSlide to Slider

--- client-server/src/app/Slider.jsx
+++ client-server/src/app/Slider.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ImageSlide from './ImageSlide.jsx';
 
+let slides = require("../assets/data/slide.json");
+//console.log("slides:", slides);
+
 class Slider extends React.Component {
 
   constructor(props) {
@@ -12,17 +15,20 @@ class Slider extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.state);
-    setInterval(this.incrementSlideCount.bind(this), 3000);
+    //console.log(this.state);
+    setInterval(this.incrementSlideCount.bind(this), slides[1].duration);
   }
 
   incrementSlideCount() {
     let newSlide = this.state.slideCount + 1;
-    if(newSlide > 3) {
+    if(newSlide > 5) {
       newSlide = 1;
     }
     this.setState({slideCount: newSlide});
-    console.log(this.state);
+    //console.log(this.state);
+    let key = "duration";
+    console.log(slides[1][key]);
+    //
   }
 
   render() {
@@ -32,6 +38,7 @@ class Slider extends React.Component {
         { this.state.slideCount === 1 ? <ImageSlide imageUrl='assets/img/yosemite.jpg' /> : null }
         { this.state.slideCount === 2 ? <ImageSlide imageUrl='assets/img/rocks.jpg' /> : null }
         { this.state.slideCount === 3 ? <ImageSlide imageUrl='assets/img/flowers.jpg' /> : null }
+        { this.state.slideCount === 4 ? <ImageSlide imageUrl='assets/data/slide.json' /> : null}
       </div>
     );
   }
