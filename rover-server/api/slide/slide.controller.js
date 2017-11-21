a6ae21fb1a2aa1bddd80e414820fec1c832d044c
Aaron <aaron@fireislandferries.com>
2017-11-21 15:33:56 -0500

    created SlideSchema

---
+++ rover-server/api/slide/slide.controller.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const jsonpatch = require('fast-json-patch');
+
+function respondWithResult(res, statusCode) {
+  statusCode = statusCode || 200;
+  return function(entity) {
+    if(entity) {
+      return res.status(statusCode).json(entity);
+    }
+    return null;
+  };
+}
+
+function patchUpdates(patches) {
+  return function(entity) {
+    try {
+      jsonpatch.apply(entity, patches, /*validate*/ true);
+    } catch(err) {
+      return Promise.reject(err);
+    }
+
+    return entity.save();
+  };
+}
+
+function removeEntity(res) {
+  return function(entity) {
+    if(entity) {
+      return entity.remove()
+        .then(() => {
+          res.status(204).end();
+        });
+    }
+  };
+}
+
+function handleEntityNotFound(res) {
+  return function(entity) {
+    if(!entity) {
+      res.status(404).end();
+      return null;
+    }
+    return entity;
+  };
+}
+
+function handleError(res, statusCode) {
+  statusCode = statusCode || 500;
+  return function(err) {
+    res.status(statusCode).send(err);
+  };
+}
+
+// Gets a list of Slides
+export function index(req, res) {
+  return Slide.find().exec()
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Gets a single Slide from the DB
+export function show(req, res) {
+  return Slide.findById(req.params.id).exec()
+    .then(handleEntityNotFound(res))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Creates a new Slide in the DB
+export function create(req, res) {
+  return Slide.create(req.body)
+    .then(respondWithResult(res, 201))
+    .catch(handleError(res));
+}
+
+// Upserts the given Slide in the DB at the specified ID
+export function upsert(req, res) {
+  if(req.body._id) {
+    delete req.body._id;
+  }
+  return Slide.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, upsert: true, setDefaultsOnInsert: true, runValidators: true}).exec()
+
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Updates an existing Slide in the DB
+export function patch(req, res) {
+  if(req.body._id) {
+    delete req.body._id;
+  }
+  return Slide.findById(req.params.id).exec()
+    .then(handleEntityNotFound(res))
+    .then(patchUpdates(req.body))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Deletes a Slide from the DB
+export function destroy(req, res) {
+  return Slide.findById(req.params.id).exec()
+    .then(handleEntityNotFound(res))
+    .then(removeEntity(res))
+    .catch(handleError(res));
+}
---
+++ rover-server/api/slide/slide.model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+var SlideSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: {
+      type: String,
+      required: true
+    },
+    fileName: String,
+    timestamp: {
+      type: Date,
+      default: Date.now
+    },
+    version: Number,
+    orientation: String,
+    size: {
+      height: Number,
+      width: Number
+    },
+  },
+
+export default mongoose.model('Slide', SlideSchema);
+
+//update for playlist model or Slides
